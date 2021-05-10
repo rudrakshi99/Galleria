ed2d38c31b940975c0db6ea8d945f1028dde99a5
rudrakshi99 <sonirudrakshi99@gmail.com>
2021-05-10 18:26:57 +0530

    Updates

---
+++ precache-manifest.efaa473c238f7091aa841c5040dea83a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3efe33636acf5cb647a891176d69b4",
+    "url": "/Galleria/index.html"
+  },
+  {
+    "revision": "4e680e9c9bccad8ddad1",
+    "url": "/Galleria/static/css/main.7523ed4f.chunk.css"
+  },
+  {
+    "revision": "5faf5e4acafbb17af9c4",
+    "url": "/Galleria/static/js/2.6ea7adf4.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/Galleria/static/js/2.6ea7adf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e680e9c9bccad8ddad1",
+    "url": "/Galleria/static/js/main.0fb8a08e.chunk.js"
+  },
+  {
+    "revision": "2a4eb3ab92da13b84dc6",
+    "url": "/Galleria/static/js/runtime-main.a2eebc84.js"
+  }
+]);
